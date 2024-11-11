6505607e5e77e999f6dc8f63c43e745ef2b118f0
akittles <151872598+akittles@users.noreply.github.com>
2024-11-11 17:27:16 -0500

    Update environment.py

--- features/environment.py
+++ features/environment.py
@@ -15,8 +15,49 @@ def browser_init(context):
     service = Service(driver_path)
     context.driver = webdriver.Chrome(service=service)
     context.app = Application(context.driver)
+
+######THIS ALSO WORKED IN HEADLESS MODE########
+    # driver_path = GeckoDriverManager().install()
+    # service = Service(driver_path)
+    # context.driver = webdriver.Firefox(service=service)
+
+    # context.driver = webdriver.Safari() #CAN ONLY RUN ON MAC OR IOS
+
+    ### HEADLESS MODE #### TESTING IN THE TERMINAL
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('headless')
+    # service = Service(ChromeDriverManager().install())
+    # context.driver = webdriver.Chrome(
+    #     options=options,
+    #     service=service
+    # )
+
+    ### BROWSERSTACK ###
+    # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
+    # bs_user = 'anthonykittles_DMlABu'
+    # bs_key = 'UUbrpL7fPHuFFe9Y7zo4'
+    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    #
+    # options = Options()
+    # bstack_options = {
+    #     "os": "Windows",
+    #     "osVersion": "11",
+    #     'browserName': 'chrome',
+    #     'sessionName': scenario_name
+    # }
+    # options.set_capability('bstack:options', bstack_options)
+    # context.driver = webdriver.Remote(command_executor=url, options=options)
+
+    ### BROWSERS WITH DRIVERS: provide path to the driver file ###--old way of doing things,
+    #           #Not needed but may fix later
+    # service = Service(executable_path='/Users/Owner/python-selenium-automation/geckodriver')
+    # context.driver = webdriver.Firefox(service=service)
+    
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
+    context.driver.wait = WebDriverWait(context.driver, 15) # aka explicit wait
+
+    context.app = Application(context.driver)
 
 
 def before_scenario(context, scenario):
